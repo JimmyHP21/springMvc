07451877036dd8369be74b59fe26552f0d520f11
JimmyHP21 <renan.henriqu@hotmail.com>
2019-06-04 01:29:38 -0300

    config login

---
+++ src/main/java/com/renan/controller/SegurancaController.java
@@ -0,0 +1,20 @@
+package com.renan.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SegurancaController {
+
+    @GetMapping("/login")
+    public String login(@AuthenticationPrincipal User user){
+
+        if(user != null) {
+            return "ja logado";
+        }
+
+        return "logado";
+    }
+}
--- src/main/java/com/renan/security/SecurityConfig.java
+++ src/main/java/com/renan/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.renan.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +18,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             withUser("admin").
             password("$2a$10$RkTVD0vVnTO9PcU2VbSOButxB3bavOmic/.cuhp4.0a9uml5Vg.bm").
             roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .and()
+            .csrf().disable();
     }
 
     @Bean
